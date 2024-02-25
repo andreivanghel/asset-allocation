a62deb336681135c6128bf77bfdc454f2dec5920
andreivanghel <andreivanghel@gmail.com>
2024-02-25 18:53:54 +0100

    merging tickers data from same asset class into one df

--- R/03_Main.R
+++ R/03_Main.R
@@ -37,9 +37,23 @@ processed_data <- preProcessing(raw_data)
 
 # export pre processed data -----------------------------------------------
 
+output_dir <- paste0(data_saving_config$processed_data_saving, "/", sourcing_date)
+
+if(!dir.exists(output_dir)){
+  dir.create(output_dir)
+}
+
 list(x = processed_data,
-     file = paste0(data_saving_config$processed_data_saving, "/", names(processed_data), "_", sourcing_date, ".csv")) %>% 
+     file = paste0(output_dir, "/", names(processed_data), "_", sourcing_date, ".csv")) %>% 
   pwalk(fwrite)
+
+
+# export single dataframe (for each asset class) --------------------------
+
+processed_data_rename <- processed_data %>% names() %>% 
+  lapply(FUN = function(market_name) processed_data[[market_name]] %>% rename(!!market_name := !!as.symbol("PRICE")))
+data <- Reduce(function(x, y) merge(x, y, by = "DATE", all = TRUE), processed_data_rename)
+
 
 
 
