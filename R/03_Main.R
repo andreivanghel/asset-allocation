693be60ed519d81126e77cf5cf9594ea542601fd
andreivanghel <andreivanghel@gmail.com>
2024-04-01 12:25:02 +0200

    pwalk data sourcing (wip)

--- R/03_Main.R
+++ R/03_Main.R
@@ -3,6 +3,11 @@
 setwd("~/Documents/GitHub/asset-allocation")
 
 
+# libraries ---------------------------------------------------------------
+
+library(purrr)
+
+
 # custom functions --------------------------------------------------------
 
 source("./R/00_DataSourcing.R")
@@ -20,8 +25,11 @@ sourcing_date <- Sys.Date()
 
 # raw data sourcing -------------------------------------------------------
 
-dataSourcing(asset_classes_config[["Equities"]], data_saving_config = data_saving_config$raw_data_saving)
+#dataSourcing(asset_classes_config[["Equities"]], data_saving_config = data_saving_config$raw_data_saving)
 
+list(assetClassConfig = asset_classes_config[asset_classes_config %>% sapply(FUN = function(x) !is_empty(x))],
+     data_saving_config = data_saving_config$raw_data_saving) %>% 
+  pwalk(dataSourcing, .progress = TRUE)
 
 # data loading ------------------------------------------------------------
 
@@ -51,9 +59,10 @@ list(x = processed_data,
 # export single dataframe (for each asset class) --------------------------
 
 processed_data_rename <- processed_data %>% names() %>% 
-  lapply(FUN = function(market_name) processed_data[[market_name]] %>% rename(!!market_name := !!as.symbol("PRICE")))
+  lapply(FUN = function(market_name) processed_data[[market_name]] %>% mutate(PRICE = as.numeric(PRICE)) %>% rename(!!market_name := !!as.symbol("PRICE")))
 data <- Reduce(function(x, y) merge(x, y, by = "DATE", all = TRUE), processed_data_rename)
 
+fwrite(data, file = "equities_2024-02-25.csv")
 
 
 
